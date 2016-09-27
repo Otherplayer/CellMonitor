6991f7be7158a42a9f477172ab2a30a04b9e65ce
郭乐乐 <lel.guo@haihangyun.com>
2016-09-27 15:01:44 +0800

    Initial Commit

---
+++ HNACellMonitor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HNACellMonitor
+//
+//  Created by __无邪_ on 16/9/27.
+//  Copyright © 2016年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
